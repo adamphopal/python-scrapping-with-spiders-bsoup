a2e5a033aecfba25c0135b78d8f21e34139d976d
hamimphopal50 <hamimphopal50@yahoo.com>
2018-11-16 10:40:31 -0800

    another spider

---
+++ third_one/splash_based/splash_based/spiders/quotes-js.py
@@ -0,0 +1,25 @@
+import scrapy
+from scrapy_splash import SplashRequest
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes-js'
+
+
+    def start_requests(self):
+        yield SplashRequest(
+            url='http://quotes.toscrape.com/js',
+            callback=self.parse,
+        )
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            item = {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('small.author::text').extract_first(), 
+                'tags': quote.css('div.tags > a.tag::text').extract(),
+            }
+          
+            
+        
+            
+            
